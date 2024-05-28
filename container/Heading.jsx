cb4fdc3ced15e541bed8f72e9a15d858df366a0e
Mangayarkarasi.S <mangaisuresh2923@gmail.com>
2024-05-28 11:15:36 +0530

    Net-profit-calc-output

--- container/Heading.jsx
+++ container/Heading.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Heading = () => {
   return (
     <div className='lg:text-center lg:pt-10 md:pt-16 lg:ml-0 md:justify-center pt-32 w-full'>
-      <h1 className="font:sans text-white font-bold text-6xl">Net Profit Calculator</h1>
+      <h1 className="font:sans text-white font-bold text-6xl ml-96">Net Profit Calculator</h1>
       <div className='flex lg:justify-center pt-4 justify-center'>
-          <div className='w-[25%]'>
-              <p className='text-white text-base'>Lorem ipsum dolor sit amet consectetur adipisicing elit.In imperdict pretium risus ut.</p>
+          <div className='w-[35%]'>
+              <p className='text-white text-xl text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit.In imperdict pretium risus ut.</p>
           </div>
       </div>
     </div>
